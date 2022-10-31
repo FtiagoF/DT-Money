6d82aec2c9a69c02d8c2c66a54a7e8f93bc6b0a4
Tiago Farias <tiagofariasnh19@gmail.com>
2022-10-31 18:28:08 -0300

    Transactions Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransctionsTable,
+} from "./styles";
 
 export function Transactions() {
-    return(
-        <div>
-            <Header />
-            <Summary />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransctionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                    R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransctionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
